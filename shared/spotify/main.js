feec392c404b7f9da65a13341b3273dfab8ef63f
Tan Hoang <tan@Tans-MacBook-Air.local>
2021-05-03 17:37:52 +0700

    ok

--- shared/spotify/main.js
+++ shared/spotify/main.js
@@ -2,6 +2,7 @@
 const menu=document.getElementById('drawer')
 const btn=document.getElementById('toggle')
 const body=document.getElementsByTagName('body')[0]
+const backdrop=document.getElementById('backdrop')
 let flag=true;
 btn.addEventListener('click',()=>{
     flag=!flag
@@ -13,6 +14,11 @@ btn.addEventListener('click',()=>{
         menu.classList="nav-responsive show"
         body.style.overflow="hidden"
     }
+})
+backdrop.addEventListener('click',()=>{
+    menu.classList="nav-responsive"
+    body.style.overflow=""
+    flag=true
 })
 window.addEventListener('resize',()=>{
     if(window.innerWidth>1092){
