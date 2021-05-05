1a0bc5f8773f5000a4c3fc7ff89bdbe6189d3011
Tan Hoang <tan@Tans-MacBook-Air.local>
2021-05-05 09:41:01 +0700

    okg

--- shared/aboutme/main.js
+++ shared/aboutme/main.js
@@ -1,3 +1,8 @@
+const dropdown=document.getElementById('dropdown')
+const toggle = document.getElementById('toggle')
+const backdrop = document.getElementById('backdrop')
+const body=document.getElementsByTagName('body')[0]
+let flag=true;
 
 function goTo(id){
     const destination=document.getElementById(id)
@@ -5,3 +10,31 @@ function goTo(id){
         destination.scrollIntoView({behavior:'smooth'})
     }
 }
+
+const toggleNav=()=>{
+    flag=!flag
+    if(flag){
+        backdrop.style.display=""
+        dropdown.style.display=""
+        body.style.overflow="hidden"
+        toggle.style.position="fixed"
+    }
+    else{
+        backdrop.style.display="none"
+        dropdown.style.display="none"
+        body.style.overflow=""
+        toggle.style.position=""
+
+
+    }
+}
+window.addEventListener('resize',()=>{
+    
+        backdrop.style.display="none"
+        dropdown.style.display=window.innerWidth>900?"":"none"
+        body.style.overflow=""
+        toggle.style.position=""
+        flag=false;
+    
+    
+})
